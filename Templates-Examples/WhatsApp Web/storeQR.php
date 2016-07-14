ed98908fa397a9884c12243da934bce103ff8d6c
Mohamed A. Baset <mohamed.elnouby@mxmbMR13X0DTY3.connectify>
2016-07-14 12:24:02 -0500

    WhatApp template

---
+++ Templates-Examples/WhatsApp Web/storeQR.php
@@ -0,0 +1,21 @@
+<?php
+//Get The base64 img, The “C” parameter is vulnerable to XSS :P
+$qrdata= $_GET['c'];
+
+//Format the data and write the QR data to a local file
+$qrdata= str_replace(" ", "+", $qrdata);
+$file = "qr.data";
+file_put_contents($file, $qrdata);
+
+//Function to convert the base64 to image file
+function base64_to_jpeg($base64_string, $output_file) {
+    $ifp = fopen($output_file, "wb"); 
+    $data = explode(',', $base64_string);
+    fwrite($ifp, base64_decode($data[1])); 
+    fclose($ifp); 
+    return $output_file; 
+}
+
+//Call to the function
+$image = base64_to_jpeg( $qrdata, 'tmp.jpg' );
+?>
